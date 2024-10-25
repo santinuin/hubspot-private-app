5df263d8db37686ea98c8d53b41d599fd8683524
Santiago Nuñez Ingas <snunez@chattigo.com>
2024-10-25 17:40:42 -0300

    get templates

---
+++ chattigo-integration/src/app/app.functions/getTemplates-function.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+
+exports.main = async (context = {}) => {
+    try {
+        //const { USERNAME, PASSWORD } = context.secrets;
+
+        // Obtener el token de autenticación
+        const loginResponse = await axios.post('https://condor-dev.chattigo.com/api-massive/message/login', {
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD
+        });
+
+        const token = loginResponse.data.access_token;
+
+
+        const decoded = jwt.decode(token);
+        const idClient = decoded.idClient;
+
+        const response = await axios.get(`https://condor-dev.chattigo.com/api-massive/message/templates/client_id/${idClient}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log("response: ", response.data);
+
+        const templates = response.data.data.map(template => ({
+            value: template.name,
+            label: template.name
+        }));
+
+        return {templates};
+    } catch (error) {
+        console.error('Error fetching options:', error);
+        return {templates: []};
+    }
+};
--- chattigo-integration/src/app/extensions/Example.jsx
+++ chattigo-integration/src/app/extensions/Example.jsx
@@ -1,75 +1,71 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  Divider,
-  Link,
-  Button,
-  Text,
-  Input,
-  Flex,
-  hubspot,
+    Button,
+    Text,
+    Input,
+    Flex,
+    hubspot, Select,
 } from "@hubspot/ui-extensions";
 
 // Define the extension to be run within the Hubspot CRM
-hubspot.extend(({ context, runServerlessFunction, actions }) => (
-  <Extension
-    context={context}
-    runServerless={runServerlessFunction}
-    sendAlert={actions.addAlert}
-  />
+hubspot.extend(({context, runServerlessFunction, actions}) => (
+    <Extension
+        context={context}
+        runServerless={runServerlessFunction}
+        sendAlert={actions.addAlert}
+    />
 ));
 
 // Define the Extension component, taking in runServerless, context, & sendAlert as props
-const Extension = ({ context, runServerless, sendAlert }) => {
-  const [text, setText] = useState("");
+const Extension = ({context, runServerless, sendAlert}) => {
+    const [text, setText] = useState("");
+    const [templates, setTemplates] = useState([{label: "Loading...", value: ""}]);
 
-  // Call serverless function to execute with parameters.
-  // The `myFunc` function name is configured inside `serverless.json`
-  const handleClick = async () => {
-    const { response } = await runServerless({ name: "myFunc", parameters: { text: text } });
-    sendAlert({ message: response });
-  };
+    useEffect(() => {
+        const fetchOptions = async () => {
+            try {
+                const response = await runServerless({ name: "getTemplates" });
+                console.log(response.response.templates);
+                if (response.response.templates) {
+                    setTemplates(response.response.templates);
+                } else {
+                    console.error("Invalid response format:", response);
+                    setTemplates([{label: "Error fetching templates", value: ""}]);
+                }
+            } catch (error) {
+                console.error("Error fetching templates:", error);
+                setTemplates([]);
+            }
+        };
+        fetchOptions();
+    }, [runServerless]);
 
-  return (
-    <>
-      <Text>
-        <Text format={{ fontWeight: "bold" }}>
-          Your first UI extension is ready!
-        </Text>
-        Congratulations, {context.user.firstName}! You just deployed your first
-        HubSpot UI extension. This example demonstrates how you would send
-        parameters from your React frontend to the serverless function and get a
-        response back.
-      </Text>
-      <Flex direction="row" align="end" gap="small">
-        <Input name="text" label="Send" onInput={(t) => setText(t)} />
-        <Button type="submit" onClick={handleClick}>
-          Click me
-        </Button>
-      </Flex>
-      <Divider />
-      <Text>
-        What now? Explore all available{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extension-components">
-          UI components
-        </Link>
-        , get an overview of{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extensions-overview">
-          UI extensions
-        </Link>
-        , learn how to{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/create-ui-extensions">
-          add a new custom card
-        </Link>
-        , jump right in with our{" "}
-        <Link href="https://developers.hubspot.com/docs/platform/ui-extensions-quickstart">
-          Quickstart Guide
-        </Link>
-        , or check out our{" "}
-        <Link href="https://github.com/HubSpot/ui-extensions-react-examples">
-          code Samples
-        </Link>
-        .
-      </Text>
-    </>
-  );
+    // Call serverless function to execute with parameters.
+    // The `myFunc` function name is configured inside `serverless.json`
+    const handleClick = async () => {
+        const {response} = await runServerless({name: "sendTemplate", parameters: {text: text}});
+        sendAlert({message: response});
+    };
+
+    return (
+        <>
+            <Text>
+                <Text format={{fontWeight: "bold"}}>
+                    Integración con Chattigo. Envio de mensajes.
+                </Text>
+                Esta es una prueba de integración con el CRM de HubSpot.
+            </Text>
+            <Flex direction="row" align="end" gap="small">
+                <Select
+                    name="templates"
+                    label="Templates"
+                    options={templates}
+                />
+                <Input name="text" label="Destinatarios" onInput={(t) => setText(t)}/>
+                <Button type="submit" onClick={handleClick}>
+                    Enviar
+                </Button>
+            </Flex>
+        </>
+    );
 };
